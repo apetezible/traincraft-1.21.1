2a24670512f25815021c212879d3dc27cf11116d
apetezible <calupriproductions@gmail.com>
2026-08-26 13:59:36 -0500

    connected solid fuelsysrem to minecraft's fuels

--- src/main/java/com/eternalblueflame/traincraft/locomotive/fuel/SolidFuelSystem.java
+++ src/main/java/com/eternalblueflame/traincraft/locomotive/fuel/SolidFuelSystem.java
@@ -1,7 +1,19 @@
 package com.eternalblueflame.traincraft.locomotive.fuel;
 
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.world.item.ItemStack;
 
+/**
+ * Solid fuel system used by steam locomotives.
+ *
+ * Any item recognized by Minecraft's fuel registry is accepted.
+ *
+ * The system deliberately does not distinguish between different kinds
+ * of solid fuel. Coal, charcoal, planks, slabs, sticks, kelp blocks,
+ * etc. are all treated according to Minecraft's own fuel registry.
+ *
+ * Solid fuel locomotives currently require water.
+ */
 public final class SolidFuelSystem implements FuelSystemDefinition {
 
     @Override
@@ -16,11 +28,21 @@ public final class SolidFuelSystem implements FuelSystemDefinition {
 
     @Override
     public boolean acceptsFuel(ItemStack stack) {
-        return false;
+        if (stack == null || stack.isEmpty()) {
+            return false;
+        }
+
+        return FuelRegistry.INSTANCE.get(stack.getItem()) != null;
     }
 
     @Override
     public int getFuelAmount(ItemStack stack) {
-        return 0;
+        if (!acceptsFuel(stack)) {
+            return 0;
+        }
+
+        Integer fuelTicks = FuelRegistry.INSTANCE.get(stack.getItem());
+
+        return fuelTicks != null ? fuelTicks : 0;
     }
 }
