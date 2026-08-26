aad37849000c0674669bd04c214c0f252272fd19
apetezible <calupriproductions@gmail.com>
2026-08-25 20:58:13 -0500

    Locomotive inventory image rendering but not correctly centered and displaying correctly

--- src/client/java/com/eternalblueflame/traincraft/client/screen/LocomotiveScreen.java
+++ src/client/java/com/eternalblueflame/traincraft/client/screen/LocomotiveScreen.java
@@ -7,30 +7,46 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
-/** Minimal container GUI for loco fuel (and water) slots. */
 public class LocomotiveScreen extends AbstractContainerScreen<LocomotiveMenu> {
-    private static final ResourceLocation TEXTURE = ResourceLocation.withDefaultNamespace(
-            "textures/gui/container/generic_54.png");
 
-    public LocomotiveScreen(LocomotiveMenu menu, Inventory playerInventory, Component title) {
+    private static final ResourceLocation TEXTURE =
+            ResourceLocation.fromNamespaceAndPath(
+                    "traincraft",
+                    "textures/gui/gui_loco_steam.png"
+            );
+
+    public LocomotiveScreen(
+            LocomotiveMenu menu,
+            Inventory playerInventory,
+            Component title
+    ) {
         super(menu, playerInventory, title);
-        this.imageHeight = 166;
-        this.inventoryLabelY = this.imageHeight - 94;
+
+        // These are the dimensions of the legacy Traincraft GUI.
+        this.imageWidth = 256;
+        this.imageHeight = 256;
     }
 
     @Override
-    protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
-        int x = (width - imageWidth) / 2;
-        int y = (height - imageHeight) / 2;
-        // Top strip (chest-style) + player inventory section
-        graphics.blit(TEXTURE, x, y, 0, 0, imageWidth, 17 + 18 * 3);
-        graphics.blit(TEXTURE, x, y + 17 + 18 * 3, 0, 126, imageWidth, 96);
-    }
+    protected void renderBg(
+            GuiGraphics guiGraphics,
+            float partialTick,
+            int mouseX,
+            int mouseY
+    ) {
+        int x = (this.width - this.imageWidth) / 2;
+        int y = (this.height - this.imageHeight) / 2;
 
-    @Override
-    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
-        renderBackground(graphics, mouseX, mouseY, partialTick);
-        super.render(graphics, mouseX, mouseY, partialTick);
-        renderTooltip(graphics, mouseX, mouseY);
+        guiGraphics.blit(
+                TEXTURE,
+                x,
+                y,
+                0,
+                0,
+                this.imageWidth,
+                this.imageHeight,
+                this.imageWidth,
+                this.imageHeight
+        );
     }
 }
